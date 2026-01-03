9b0d717f24af79b163f2bc905b85c55ad2b1ce57
nour-allah-khaled <nourallahk7@gmail.com>
2026-01-04 00:41:13 +0200

    fix executionType and env

--- src/main/java/com/swaglabs/android/DriverManager.java
+++ src/main/java/com/swaglabs/android/DriverManager.java
@@ -62,6 +62,9 @@ public class DriverManager {
         }
     }
     private  static String getAppiumPath() {
+        if ("ci".equalsIgnoreCase(PropertyReader.getProperty("executionType"))) {
+            return null;
+        }
         return "C:\\Users\\Dell\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js";
     }
     private static String getServiceIpAddress() {
