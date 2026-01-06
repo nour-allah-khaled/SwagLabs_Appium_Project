4c8af64fc7ae7bcc49bc1a8372e26657cf23c7d3
nour-allah-khaled <nourallahk7@gmail.com>
2026-01-07 00:36:51 +0200

    add: e2e Tcs

--- src/test/java/e2e/E2E.java
+++ src/test/java/e2e/E2E.java
@@ -10,18 +10,20 @@ import tests.BaseClass;
 @Severity(SeverityLevel.CRITICAL)
 public class E2E extends BaseClass {
     @Description("Comprehensive end-to-end test scenarios covering complete user journeys")
-    // Login -> Home (multiple products) -> Add to Cart -> Checkout -> Overview -> Finish -> Home
+    @Epic("End-to-End Tests")
+   // Login > Home
     @Test
-    public void LoginToFinsh() {
+    public void LoginTest(){
         new Page01_Login(DriverManager.getDriver()).userName(getUserName())
                 .passWord(getPassword()).loginBtn().assertLogin();
-        new Page02_Home(DriverManager.getDriver()).AddAllProductsToCart(getProductOne(), getProductTwo()).ClickOnCartIcon().assertToAddToCartPage();
-        new Page04_AddToCart(DriverManager.getDriver()).clickCheckout().assertToCheckoutPage();
-        new Page05_CheckOut(DriverManager.getDriver()).enterFirstName(getFirstName())
-                .enterLastName(getLastName()).enterPostalCode(getZipCode())
-                .clickContinue().assertToOverview();
-        new Page06_Overview(DriverManager.getDriver()).clickFinish().assertToFinishPage();
-        new Page07_Finish(DriverManager.getDriver()).clickBackHome().assertToHome();
+    }
+    // Login -> Home -> Product Details
+    @Test
+    public void LoginToProductDetails(){
+        new Page01_Login(DriverManager.getDriver()).userName(getUserName())
+                .passWord(getPassword()).loginBtn().assertLogin();
+        new Page02_Home(DriverManager.getDriver()).ClickOnSpecificProductTitle()
+                .assertToProductDetails();
     }
     // Login -> Home (with sorting product Name) -> Add to Cart -> Checkout > Overview > Finish > Home
     @Test
@@ -30,6 +32,21 @@ public class E2E extends BaseClass {
                 .passWord(getPassword()).loginBtn().assertLogin();
         new Page02_Home(DriverManager.getDriver()).filterProducts("Name (A to Z)").assertNamesSortedAToZ()
                 .AddAllProductsToCart(getProductOne(), getProductTwo()).ClickOnCartIcon().assertToAddToCartPage();
+        new Page04_AddToCart(DriverManager.getDriver()).clickCheckout().assertToCheckoutPage();
+        new Page05_CheckOut(DriverManager.getDriver()).enterFirstName(getFirstName())
+                .enterLastName(getLastName()).enterPostalCode(getZipCode())
+                .clickContinue().assertToOverview();
+        new Page06_Overview(DriverManager.getDriver()).clickFinish().assertToFinishPage();
+        new Page07_Finish(DriverManager.getDriver()).clickBackHome().assertToHome();
+    }
+    // Login -> Home -> Remove Product From Cart -> Add to Cart -> Checkout > Overview > Finish > Home
+    @Test
+    public void LoginToRemoveProductFromCart() {
+        new Page01_Login(DriverManager.getDriver()).userName(getUserName())
+                .passWord(getPassword()).loginBtn().assertLogin();
+        new Page02_Home(DriverManager.getDriver())
+                .AddAllProductsToCart(getProductOne(), getProductTwo()).
+                removeSelectedProductsFromCart(getProductOne()).assertCartBadgeCount().ClickOnCartIcon().assertToAddToCartPage();;
         new Page04_AddToCart(DriverManager.getDriver()).clickCheckout().assertToCheckoutPage();
         new Page05_CheckOut(DriverManager.getDriver()).enterFirstName(getFirstName())
                 .enterLastName(getLastName()).enterPostalCode(getZipCode())
@@ -60,6 +77,58 @@ public class E2E extends BaseClass {
                 .assertToProductDetails();
         new Page03_ProductDetails(DriverManager.getDriver()).clickAddToCart().assertCartBadgeState("added")
                 .clickonCartIcon().assertToAddToCartPage();
+        new Page04_AddToCart(DriverManager.getDriver()).clickCheckout().assertToCheckoutPage();
+        new Page05_CheckOut(DriverManager.getDriver()).enterFirstName(getFirstName())
+                .enterLastName(getLastName()).enterPostalCode(getZipCode())
+                .clickContinue().assertToOverview();
+        new Page06_Overview(DriverManager.getDriver()).clickFinish().assertToFinishPage();
+        new Page07_Finish(DriverManager.getDriver()).clickBackHome().assertToHome();
+    }
+    // Login -> Home -> Product Details -> Add/Remove to Cart -> Add to Cart Page
+    @Test
+    public void LoginToAddProductToCartPage()
+    {
+        new Page01_Login(DriverManager.getDriver()).userName(getUserName())
+                .passWord(getPassword()).loginBtn().assertLogin();
+        new Page02_Home(DriverManager.getDriver()).AddAllProductsToCart(getProductOne())
+                .ClickOnSpecificProductTitle().assertToProductDetails();
+        new Page03_ProductDetails(DriverManager.getDriver()).clickRemoveFromCart()
+                .assertCartBadgeState("removed").clickAddToCart()
+                .assertCartBadgeState("added").clickonCartIcon().assertToAddToCartPage();
+    }
+    // Login -> Home -> Product Details -> Add to Cart -> Checkout Page
+    @Test
+    public void LoginToProceedToCheckoutFromCartPage()
+    {
+        new Page01_Login(DriverManager.getDriver()).userName(getUserName())
+                .passWord(getPassword()).loginBtn().assertLogin();
+        new Page02_Home(DriverManager.getDriver()).ClickOnSpecificProductTitle()
+                .assertToProductDetails();
+        new Page03_ProductDetails(DriverManager.getDriver()).clickAddToCart().assertCartBadgeState("added")
+                .clickonCartIcon().assertToAddToCartPage();
+        new Page04_AddToCart(DriverManager.getDriver()).clickCheckout().assertToCheckoutPage();
+    }
+    // Login -> Home -> Product Details -> Add to Cart -> Checkout -> Overview
+    @Test
+    public void LoginToProductDetailsToOverviewToFinish(){
+        new Page01_Login(DriverManager.getDriver()).userName(getUserName())
+                .passWord(getPassword()).loginBtn().assertLogin();
+        new Page02_Home(DriverManager.getDriver()).ClickOnSpecificProductTitle()
+                .assertToProductDetails();
+        new Page03_ProductDetails(DriverManager.getDriver()).clickAddToCart().assertCartBadgeState("added")
+                .clickonCartIcon().assertToAddToCartPage();
+        new Page04_AddToCart(DriverManager.getDriver()).clickCheckout().assertToCheckoutPage();
+        new Page05_CheckOut(DriverManager.getDriver()).enterFirstName(getFirstName())
+                .enterLastName(getLastName()).enterPostalCode(getZipCode())
+                .clickContinue().assertToOverview();
+        new Page06_Overview(DriverManager.getDriver()).clickFinish().assertToFinishPage();
+    }
+    // Login -> Home (multiple products) -> Add to Cart -> Checkout -> Overview -> Finish -> Home
+    @Test
+    public void LoginToFinsh() {
+        new Page01_Login(DriverManager.getDriver()).userName(getUserName())
+                .passWord(getPassword()).loginBtn().assertLogin();
+        new Page02_Home(DriverManager.getDriver()).AddAllProductsToCart(getProductOne(), getProductTwo()).ClickOnCartIcon().assertToAddToCartPage();
         new Page04_AddToCart(DriverManager.getDriver()).clickCheckout().assertToCheckoutPage();
         new Page05_CheckOut(DriverManager.getDriver()).enterFirstName(getFirstName())
                 .enterLastName(getLastName()).enterPostalCode(getZipCode())
